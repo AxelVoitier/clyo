66290c35c2afd0e92b21fbd07f9321dc2e05d6ab
Axel Voitier <axel.voitier@gmail.com>
2023-11-17 21:35:42 +0100

    Applying py-project template 1.1.0 (switch to pyproject.toml)

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Clyo
 
+[![PyPI - Version](https://img.shields.io/pypi/v/clyo.svg)](https://pypi.org/project/clyo)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/clyo.svg)](https://pypi.org/project/clyo)
+
 Give your Python scripts various flavours of CLI!
+
+-----
+
+**Table of Contents**
+
+- [Installation](#installation)
+- [License](#license)
+
+## Installation
+
+```console
+pip install clyo
+```
+
+## License
+
+`clyo` is distributed under the terms of the [MPLv2](https://spdx.org/licenses/MPL-2.0.html) license.
--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -9,19 +9,21 @@ nav = mkdocs_gen_files.Nav()
 
 for path in sorted(Path('clyo').rglob("*.py")):
     module_path = path.with_suffix("")
-    doc_path = path.with_suffix(".md")
+    doc_path = path.relative_to('clyo').with_suffix(".md")
     full_doc_path = Path("reference", doc_path)
 
     parts = list(module_path.parts)
 
     if parts[-1] == "__init__":
-        parts = parts[:-1]
+        nav_parts = parts = parts[:-1]
         doc_path = doc_path.with_name('index.md')
         full_doc_path = full_doc_path.with_name('index.md')
     elif parts[-1] == "__main__":
         continue
+    else:
+        nav_parts = parts[1:]
 
-    nav[parts] = doc_path.as_posix()
+    nav[nav_parts] = doc_path.as_posix()
 
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:
         identifier = ".".join(parts)
